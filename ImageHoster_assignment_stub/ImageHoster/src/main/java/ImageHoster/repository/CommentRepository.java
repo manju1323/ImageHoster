01dec1b53142c1271697c808458925475b5ede7b
manju1323 <62888077+manju1323@users.noreply.github.com>
2020-10-26 21:39:28 +0530

    Add files via upload

---
+++ ImageHoster_assignment_stub/ImageHoster/src/main/java/ImageHoster/repository/CommentRepository.java
@@ -0,0 +1,31 @@
+package ImageHoster.repository;
+
+import ImageHoster.model.Comment;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceUnit;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CommentRepository {
+    @PersistenceUnit(unitName = "imageHoster")
+    private EntityManagerFactory emf;
+
+    public Comment insertComment(Comment newComment) {
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+
+        try {
+            transaction.begin();
+            em.persist(newComment);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        }
+        return newComment;
+    }
+
+
+}
